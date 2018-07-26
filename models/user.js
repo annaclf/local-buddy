3d2325150b8e6098a7a4eb8ba9878243a336753c
hcorta <hugocorta@hotmail.com>
2018-07-26 20:55:10 +0200

    Added: model user.js

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ const userSchema = new Schema({
   email: { type: String, required: true , unique: true },
   biography: { type: String, required: true , min: 0, max: 600 },
   city: { type: String, required: true },
-  avatarUrl: { type: String, default: 'images/default-avatar.png' }
+  avatarUrl: { type: String, default: '../public/images/default-avatar.jpg' }
 });
 
 const User = mongoose.model('User', userSchema);

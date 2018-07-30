af5170e1e12a38de22a0f35eaf3048ae0043988d
hcorta <hugocorta@hotmail.com>
2018-07-30 09:29:52 +0200

    Update Hugo Files

--- app.js
+++ app.js
@@ -58,11 +58,11 @@ app.use('/buddies', buddiesRouter);
 // error handler
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/auth.js
+++ routes/auth.js
@@ -5,85 +5,82 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-
-//SIGN UP: --------------------------------------------------------------
+// SIGN UP: --------------------------------------------------------------
 
 router.get('/signup', (req, res, next) => {
   let numbers = [];
-  for(let i = 18; i < 100; i++){
+  for (let i = 18; i < 100; i++) {
     numbers.push(i);
   }
 
-  res.render('auth/signup', {numbers})
+  res.render('auth/signup', {numbers});
 });
 
 router.post('/signup', (req, res, next) => {
   const { username, password, email, fullname, city, age, category, highlights, biography, bedsNumber, typeBeds } = req.body;
   let { avatarUrl, transport } = req.body;
-  if(!username || !password) {return res.render('auth/signup', {message: 'Incorrect! Please, try again'})}
+  if (!username || !password) { return res.render('auth/signup', {message: 'Incorrect! Please, try again'}); }
 
   User.findOne({ username })
-  .then(user => {
-    if(user){
-      res.render('auth/signup', {message: 'Already existing user'});
-    } else {
-      const salt = bcrypt.genSaltSync(saltRounds);
-      const hashedPassword = bcrypt.hashSync(password, salt);
+    .then(user => {
+      if (user) {
+        res.render('auth/signup', {message: 'Already existing user'});
+      } else {
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashedPassword = bcrypt.hashSync(password, salt);
 
-      if(!avatarUrl) avatarUrl = '/images/default-avatar.jpg';
-      if(!transport) transport = 'none';
+        if (!avatarUrl) avatarUrl = '/images/default-avatar.jpg';
+        if (!transport) transport = 'none';
 
-      const newUser = new User({ username, password, email, fullname, city, age, category, highlights, biography, bedsNumber, typeBeds, transport, avatarUrl });
-      req.session.currentUser = newUser;
-      return newUser.save();
-    }
-  })
-  .then( () => {
-    res.redirect('/')
-  })
-  .catch(error => {
-    next(error)
-  })
+        const newUser = new User({ username, password, email, fullname, city, age, category, highlights, biography, bedsNumber, typeBeds, transport, avatarUrl });
+        req.session.currentUser = newUser;
+        return newUser.save();
+      }
+    })
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 
-
-//LOGIN: --------------------------------------------------------------
+// LOGIN: --------------------------------------------------------------
 
 router.get('/login', (req, res, next) => {
-  res.render('auth/login')
+  res.render('auth/login');
 });
-
 
 router.post('/login', (req, res, next) => {
   const { username, password } = req.body;
-  if( !username || !password ) return res.render('auth/login', { message: 'You have to fill all the fields' });
+  if (!username || !password) return res.render('auth/login', { message: 'You have to fill all the fields' });
 
   User.findOne({ username })
-  .then(user => {
-    if(!user){
-      return res.render('auth/login', {message: 'User or password are incorrect'})
-    }
-    if (bcrypt.compareSync(password, user.password)) {
+    .then(user => {
+      if (!user) {
+        return res.render('auth/login', {message: 'User or password are incorrect'});
+      }
+      if (bcrypt.compareSync(password, user.password)) {
       // Save the login in the session!
-      req.session.currentUser = user;
+        req.session.currentUser = user;
 
-      console.log(`logged in as ${user.username}`);
+        console.log(`logged in as ${user.username}`);
 
-      return res.redirect('/');
-    } else {
-      return res.render('auth/login', {message: 'Incorrect password, please try again!'})
-    }
-  })
-  .catch(error => {
-    next(error);
-  })
-})
+        return res.redirect('/');
+      } else {
+        return res.render('auth/login', {message: 'Incorrect password, please try again!'});
+      }
+    })
+    .catch(error => {
+      next(error);
+    });
+});
 
-//LOG OUT: --------------------------------------------------------------
+// LOG OUT: --------------------------------------------------------------
 
 router.post('/logout', (req, res, next) => {
   delete req.session.currentUser;
   res.redirect('/');
-})
+});
 
 module.exports = router;

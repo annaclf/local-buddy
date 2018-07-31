25cdb97e76a8adcb1d5a84b811c42757cbe29e5c
Anna Fredriksson <leliloro@gmail.com>
2018-07-31 21:08:11 +0200

    Add description to route index

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const router = express.Router();
 /* GET home page. */
 
 router.get('/', (req, res, next) => {
-  User.find({},{ fullname: 1, city: 1, highlights: 1 })
+  User.find({},{ fullname: 1, city: 1, highlights: 1, biography: 1 })
     .then(user => {
       console.log(user);
       res.render('index', { user });

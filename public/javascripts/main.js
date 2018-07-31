f9fe9ac40396be31a4a60d34e420a66261b22058
Anna Fredriksson <leliloro@gmail.com>
2018-07-31 12:15:13 +0200

    fix: navbar organisation

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,45 +8,24 @@ function main () {
     mousewheelControl: true,
     pagination: {
       el: '.swiper-pagination',
-      draggable:true
+      draggable: true
     },
     breakpoints: {
       1024: {
         slidesPerView: 3,
-        spaceBetween: 10,
+        spaceBetween: 10
       },
       768: {
         slidesPerView: 3,
-        spaceBetween: 10,
+        spaceBetween: 10
       },
       420: {
         slidesPerView: 1,
-        spaceBetween: 10,
+        spaceBetween: 10
       }
     }
   });
-
-  /**Esto es para hacer la validación de las fechas del calendario */
-
-  // if (buddieProfile) {
-  //   validateDate();
-  // }
-  // function validateDate () {
-  //   const dateInputs = document.querySelectorAll('input[type="date"]');
-  //   dateInputs.forEach(input => {
-  //     input.value = moment().format('YYYY-MM-DD');
-  //   });
-  //   document.getElementById('book-buddy').addEventListener('click', function () {
-  //     const from = document.getElementById('startDate').value;
-  //     const to = document.getElementById('endDate').value;
-  //     if (moment(from).isAfter(moment(to))) {
-  //       console.log('end date must be higher than start date');
-  //       return false;
-  //     }
-  //   });
-  // }
-  
-  const buddieProfile = document.getElementById('buddie-profile');
+  // const buddieProfile = document.getElementById('buddie-profile');
   const menuTrigger = document.getElementById('menu_trigger');
   const menuContainer = document.getElementById('menu-container');
 
@@ -54,18 +33,18 @@ function main () {
     menuTrigger.classList.toggle('open');
     menuContainer.classList.toggle('open');
   });
-
+  
 }
 
 window.addEventListener('load', main);
 
+// const App = {
+//   clickmenu : () => {
+//     this.menuTrigger.addEventListener('click', () => {
+//       this.menuTrigger.classList.toggle('open');
+//       this.menuContainer.classList.toggle('open');
+//     });
+//   }
+// };
 
-const App = {
-  
-  clickmenu : () => {
-    this.menuTrigger.addEventListener('click', () => {
-      this.menuTrigger.classList.toggle('open');
-      this.menuContainer.classList.toggle('open');
-    });
-  }
-};
+/* Esto es para hacer la validación de las fechas del calendario */

2919f2def5cd44a1f8a44f35b8432bef482ed3ec
hcorta <hugocorta@hotmail.com>
2018-08-01 16:34:00 +0200

    commit pal pull

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,8 @@ const router = express.Router();
 /* GET home page. */
 
 router.get('/', (req, res, next) => {
-  User.find({}, { fullname: 1, city: 1, highlights: 1, biography: 1 })
+  User.find()
     .then(user => {
-      console.log(user);
       res.render('index', { user });
     })
     .catch(error => {
